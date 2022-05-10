8547d182242f9cb1c388950d330c2d13a63bdd8c
sethukumar0001 <sethukumar0001@gmail.com>
2022-05-10 12:25:38 +0530

    ui fixed

--- src/components/VerticalSlider/index.js
+++ src/components/VerticalSlider/index.js
@@ -126,7 +126,10 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    margin: '8%',
+    marginLeft: '5%',
+    marginRight:'8%',
+    marginTop:'2%',
+    marginBottom:'4%'
   },
   rating: {
     flexDirection: 'row',
